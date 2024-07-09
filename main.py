1979032fd2ca0eb7c2d9d7f9e6c5a1c6cc6f67ce
VojtechLuber <vojtaluber@seznam.cz>
2024-07-09 21:53:38 +0200

    Add header and sum function and complete columns

--- main.py
+++ main.py
@@ -6,11 +6,56 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx*")
 
 for filepaths in filepaths:
-    df = pd.read_excel(filepaths,sheet_name="Sheet 1")
+
+
     pdf = FPDF (orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepaths).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50,h=8,txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50,h=8,txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepaths, sheet_name="Sheet 1")
+
+    # add a header
+    collumns = df.columns
+    collumns = [item.replace("_"," ").title() for item in collumns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=collumns[0], border=1)
+    pdf.cell(w=70, h=8, txt=collumns[1], border=1)
+    pdf.cell(w=30, h=8, txt=collumns[2], border=1)
+    pdf.cell(w=30, h=8, txt=collumns[3], border=1)
+    pdf.cell(w=30, h=8, txt=collumns[4], border=1, ln=1)
+
+
+    # add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
     pdf.output(f"PDFs/{filename}.pdf")
+
